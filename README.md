150b0c079646613e0645e76b039ecd6ac9f62f5e
coypan <coypan@tencent.com>
2020-02-09 11:51:43 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,9 @@
+### 一个简单的cli
+下载一个简单的[react项目模板](https://github.com/CoyPan/easy-template-react)到本地
+
+### 使用方法
+
+1. 全局安装cli
+```npm install -g easy-cli-react```
+
+2. 执行```easy-cli-react ${projectName}```
---
+++ index.js
@@ -0,0 +1,69 @@
+const commander = require('commander');
+const chalk = require('chalk');
+const fs = require('fs-extra');
+const path = require('path');
+const request = require('request');
+const progress = require('request-progress');
+const exec = require('child_process').exec;
+
+const program = new commander.Command();
+let projectName = undefined;
+
+// 初始化命令行
+program
+    .name('easy-cli-react')
+    .version('0.0.1')
+    .description('Init a react project using easy-template-react')
+    .arguments('<projectName>')
+    .action(name => {
+        projectName = name;
+    })
+    .parse(process.argv);
+
+if (!projectName) {
+    console.log(chalk.red('[Error]: need projectName'));
+    process.exit();
+}
+
+// 开始下载项目模板
+console.log(chalk.cyan('[easy-cli-react]start to download project template...'));
+const templateUrl = 'https://github.com/CoyPan/easy-template-react/archive/master.zip';
+
+const downloadZipName = 'template.zip';
+progress(request(templateUrl))
+    .on('progress', function (state) {
+        console.log(chalk.cyan(`[easy-cli-react]downloading project template (${Math.floor(state.percent || 0) * 100 + '%'})`));
+    })
+    .on('error', function (err) {
+        console.log(chalk.red(`[error]${err}`));
+    })
+    .on('end', function () {
+
+        console.log(chalk.cyan('[easy-cli-react]downloading project template (100%)'));
+        console.log(chalk.cyan('[easy-cli-react]start to init project...'));
+
+        // 解压并且重命名文件夹
+        const cmdStr = [
+            `unzip -o ${downloadZipName} -d ./`,
+            `rm ${downloadZipName}`,
+            `mv easy-template-react-master ${projectName}`
+        ].join(' && ');
+
+        exec(cmdStr, (error, stdout, stderr) => {
+            if (error) {
+                console.log(chalk.red(`[Error]: ${error}`));
+                process.exit();
+            }
+
+            // 修改package.json内的项目名
+            const packageJson = fs.readJsonSync(`${projectName}/package.json`);
+            packageJson.name = projectName;
+            fs.writeFileSync(
+                `${projectName}/package.json`, 
+                JSON.stringify(packageJson, null, 4)
+            );
+            console.log(chalk.cyan('[easy-cli-react]done!!'));
+
+        });
+    })
+    .pipe(fs.createWriteStream(downloadZipName));
