71ae66904c550ee0b4d3c74149a0f3822814b9f3
coypan <coypan@tencent.com>
2020-02-09 21:59:40 +0800

    fix download progress

--- index.js
+++ index.js
@@ -33,7 +33,8 @@ const templateUrl = 'https://github.com/CoyPan/easy-template-react/archive/maste
 const downloadZipName = 'template.zip';
 progress(request(templateUrl))
     .on('progress', function (state) {
-        console.log(chalk.cyan(`[easy-cli-react]downloading project template (${Math.floor(state.percent || 0) * 100 + '%'})`));
+        const progress = Math.floor(state.percent || 0) * 100 + '%';
+        console.log(chalk.cyan(`[easy-cli-react]downloading project template (${progress})`));
     })
     .on('error', function (err) {
         console.log(chalk.red(`[error]${err}`));
